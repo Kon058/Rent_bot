1f0779b76e77c681fa43138680dade053036841c
Kon058 <popovishaleksiy@mail.ru>
2022-11-22 10:31:07 +0300

    Delete token

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 token = os.environ.get('TOKEN8')
 
-#token = '5038240909:AAGK8aF8V3M73EYrpB1K_q6BqIe8T7vqz6k' #668
+
 bot = telebot.TeleBot(token)
 
 text_start = 'ТЦ Березка\n\nКоличество оплативших -\nКоличество не оплативших -'
